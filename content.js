145bfd63163353ae53b1356d15cc9b1fc4988fbb
Raigo <raigo.jurisoo@gmail.com>
2023-06-03 19:46:44 +0300

    images now viewable in the extension

--- content.js
+++ content.js
@@ -3,30 +3,40 @@
 document.body.style.backgroundColor = "lightblue";
 // Find all the image elements on the page
 // Find all the image elements on the page
-imageElements = document.querySelectorAll("img");
+function findNearestText(imageElement) {
+  let nearestText = null;
+  let currentNode = imageElement.parentNode;
 
-if (imageElements.length > 0) {
-	const imageTextPairs = [];
+  while (currentNode && currentNode !== document.body) {
+    if (currentNode.innerText.trim() !== "") {
+      nearestText = currentNode.innerText.trim();
+      break;
+    }
+    currentNode = currentNode.parentNode;
+  }
 
-	// Loop through each image element
-	for (const imageElement of imageElements) {
-		let nearestText = null;
-		let currentNode = imageElement.parentNode;
-
-		while (currentNode && currentNode !== document.body) {
-			if (currentNode.innerText.trim() !== "") {
-				nearestText = currentNode.innerText.trim();
-				break;
-			}
-			currentNode = currentNode.parentNode;
-		}
-
-		const imageURL = imageElement.src;
-		imageTextPairs.push({ image: imageURL, text: nearestText });
-	}
-
-	// Send the image-text pairs to the background script
-	chrome.runtime.sendMessage({ imageTextPairs });
-} else {
-	console.log("No image elements found on the webpage.");
+  return nearestText;
 }
+
+// Message listener to handle "getData" message from the popup
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.action === "getData") {
+    const imageElements = document.querySelectorAll("img");
+
+    if (imageElements.length > 0) {
+      const imageTextPairs = [];
+
+      // Loop through each image element
+      for (const imageElement of imageElements) {
+        const imageURL = imageElement.src;
+        const nearestText = findNearestText(imageElement);
+        imageTextPairs.push({ image: imageURL, text: nearestText });
+      }
+
+      // Send the image-text pairs to the popup
+      sendResponse({ imageTextPairs });
+    } else {
+      console.log("No image elements found on the webpage.");
+    }
+  }
+});
