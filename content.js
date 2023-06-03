2a97708d214e2fa6823ca779c00903fe55d1a3e1
Raigo <raigo.jurisoo@gmail.com>
2023-06-03 16:08:10 +0300

    template for the chrome extension

---
+++ content.js
@@ -0,0 +1,6 @@
+// content.js
+
+// This script will run on the webpage you want to modify or interact with.
+
+// Example: Change the background color of the webpage
+document.body.style.backgroundColor = 'lightblue';
---
+++ popup.js
@@ -0,0 +1,8 @@
+// popup.js
+
+// This script will run when the extension's popup is opened.
+
+// Example: Display a message when the popup is opened
+document.getElementById('message').textContent = 'Hello, Chrome Extension!';
+
+
