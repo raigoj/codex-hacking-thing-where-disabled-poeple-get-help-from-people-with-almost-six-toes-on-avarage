20d56107e09d93d62fe64d923ed95a7262f58881
Raigo <raigo.jurisoo@gmail.com>
2023-06-03 16:40:06 +0300

    added .env file

--- content.js
+++ content.js
@@ -1,4 +1,6 @@
-// content.js
+require('dotenv').config();
+const apiToken = process.env.API_TOKEN;
+// Use the `apiToken` variable in your API calls or wherever needed
 
 // This script will run on the webpage you want to modify or interact with.
 
