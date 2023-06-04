db73ce1c7a70b7e844a35c4883b1a0b10b467a0a
CitaZane <zanekrumina2@gmail.com>
2023-06-04 12:36:38 +0300

    refactored fetch

--- background.js
+++ background.js
@@ -1,36 +1,38 @@
 // Listen for messages from the content script
 //require('dotenv').config();
-chrome.runtime.onMessage.addListener(function (data, request) {
-	// Access the received images and text data.image, data.text
-	console.log("wait...");
-	apiKey = chrome.storage.local.get("userInput", function (result) {
-		console.log(result.userInput);
-		return result.userInput.trim();
-	});
-	fetch("https://api.openai.com/v1/chat/completions", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-			Authorization: `Bearer ${apiKey}`,
-		},
-		body: JSON.stringify({
-			model: "gpt-4",
-			messages: [
-				{
-					role: "user",
-					content: `Give me in a few words a good alt for this image: ${data.image} based on this text: ${data.text}. In case wher e text is not relavent to image provide alt text only based on image.`,
-				},
-			],
-			max_tokens: 50,
-		}),
-	})
-		.then((response) => response.json())
-		.then((data) => {
-			// Handle the response data
-			console.log("Got response", data);
-		})
-		.catch((error) => {
-			// Handle any errors
-			console.error("Error:", error);
-		});
+let result = chrome.runtime.onMessage.addListener(function (data, request) {
+  chrome.storage.local.get("userInput", function (result) {
+    apiKey = result.userInput
+    getGPTResponse(apiKey, data)
+      .then(res => console.log(res))
+  });
 });
+
+function getGPTResponse(apiKey, data) {
+  return fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify({
+      model: "gpt-4",
+      messages: [
+        {
+          role: "user",
+          content: `Give me in a few words a good alt for this image: ${data.image} based on this text: ${data.text}. In case wher e text is not relavent to image provide alt text only based on image.`,
+        },
+      ],
+      max_tokens: 50,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // Handle the response data
+      return data
+    })
+    .catch((error) => {
+      // Handle any errors
+      console.error("Error:", error);
+    });
+}
